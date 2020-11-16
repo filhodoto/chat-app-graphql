8aa993cc685d9fc116987247aa53dea6bee00289
Goncalo Ramalho <meet@goncaloramalho.com>
2020-11-16 15:04:50 +0000

    Create GraphQL server

---
+++ server/server.js
@@ -0,0 +1,46 @@
+import { GraphQLServer } from 'graphql-yoga';
+
+// Empty array we will fill with the messages
+const messages = [];
+
+// Define GraphQL type definitions
+const typeDefs = `
+    type Message {
+        id: ID!
+        username: String!
+        content: String!
+    }
+
+    type Query {
+        messages: [Message!]
+    }
+
+    type Mutation {
+        postMessage(username: String!, content: String!): ID!
+    }
+`;
+
+// Define how we are gonna get the data
+const resolvers = {
+  Query: { messages: () => messages },
+  Mutation: {
+    postMessage: (parent, { username, content }) => {
+      // Create and ID based on messages length (which won't be repeat)
+      const id = messages.length;
+
+      const message = { id, username, content };
+
+      // Add message to messages array
+      messages.push(message);
+
+      return id;
+    },
+  },
+};
+
+// Create new server
+const server = new GraphQLServer({ typeDefs, resolvers });
+
+server.start(({ port }) => {
+  console.log(`Server started at http://localhost:${port}/`);
+});
